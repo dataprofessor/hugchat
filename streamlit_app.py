cea8e5521c7fc08d66ac6865067c243e32015a2f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-05 22:09:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,42 @@
 import streamlit as st
+from streamlit_chat import message
 from hugchat import hugchat
 
 st.title('🤗💬 HugChat App')
 
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
 
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+def query(input):
+    response = chatbot.chat(input)
+    return response
+
+user_input = get_text()
 chatbot = hugchat.ChatBot()
+
+if user_input:
+    output = query({
+        "inputs": {
+            "past_user_inputs": st.session_state.past,
+            "generated_responses": st.session_state.generated,
+            "text": user_input,
+        },"parameters": {"repetition_penalty": 1.33},
+    })
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output["generated_text"])
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+
+
