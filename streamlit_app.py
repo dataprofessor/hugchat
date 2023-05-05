e5085d55181dc07cbe93aee7fc4470684c722905
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-05 22:27:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,13 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    input_text = container.text_input("You: ","Hello, how are you?", key="input")
     return input_text 
 
 def query(prompt):
     response = chatbot.chat(prompt)
     return response
+
 
 user_input = get_text()
 chatbot = hugchat.ChatBot()
@@ -32,5 +33,5 @@ if st.session_state['generated']:
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
-
+container = st.container()
 
