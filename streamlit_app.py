e31cbfe0ac2d45f287d9d5d9321f086a8c1d8c0f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-06 17:24:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,29 +10,30 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
+# Location of input/response containers
+input_container = st.container()
+response_container = st.container()
+
+# User input
 def get_text():
     input_text = st.text_input("You: ","Hello", key="input")
     return input_text
+
+with input_container:
+    user_input = get_text()
+
+# Response output
 def query(prompt):
     chatbot = hugchat.ChatBot()
     response = chatbot.chat(prompt)
     return response
 
-# Location of input/response containers
-input_container = st.container()
-response_container = st.container()
-
-with input_container:
-    user_input = get_text()
-    
-
 with response_container:
     if user_input:
         response = query(user_input)
-
         st.session_state.past.append(user_input)
         st.session_state.generated.append(response)
-
+        
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])-1, -1, -1):
             message(st.session_state["generated"][i], key=str(i))
