146e39013a8428c17f4474931b2f731275441486
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-07 00:35:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 
+# Sidebar contents
 with st.sidebar:
     st.title('🤗💬 HugChat App')
     st.markdown('''
@@ -17,53 +18,43 @@ with st.sidebar:
     add_vertical_space(5)
     st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
 
-# Generate empty lists for generated and past values
+# Generate empty lists for generated and past.
+## generated stores AI generated responses
 if 'generated' not in st.session_state:
     st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+## past stores User's questions
 if 'past' not in st.session_state:
     st.session_state['past'] = ['Hi!']
-###if 'text_input' not in st.session_state:
-    ###st.session_state['text_input'] = ['Hi!']
-    
-# Location of input/response containers
+
+# Layout of input/response containers
 input_container = st.container()
-colored_header(
-    label='',
-    description='',
-    color_name='blue-30')
+colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
 # User input
-###def text_submit():
-    ###st.session_state.text_input = st.session_state.input
-    ###st.session_state.input = ''
+## Function for taking user provided prompt as input
 def get_text():
     input_text = st.text_input("You: ", "", key="input")
     return input_text
-
+## Applying the user input box
 with input_container:
     user_input = get_text()
-    ###st.text_input("You: ", "", key="input", on_change=text_submit)
-    
+
 # Response output
+## Function for taking user prompt as input followed by producing AI generated responses
 def query(prompt):
     chatbot = hugchat.ChatBot()
     response = chatbot.chat(prompt)
     return response
 
+## Conditional display of AI generated responses as a function of user provided prompts
 with response_container:
     if user_input:
-    ###if st.session_state.text_input:
         response = query(user_input)
         st.session_state.past.append(user_input)
-        
-        ###response = query(st.session_state.text_input)
-        ###st.session_state.past.append(st.session_state.text_input)
         st.session_state.generated.append(response)
         
     if st.session_state['generated']:
-        #for i in range(len(st.session_state['generated'])-1, -1, -1):
         for i in range(len(st.session_state['generated'])):
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
             message(st.session_state["generated"][i], key=str(i))
-
