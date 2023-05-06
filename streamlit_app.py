fd172f6de3e315c6603bc87fdedfe07c5901edd4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-06 16:12:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,13 +27,12 @@ chatbot = hugchat.ChatBot()
 
 with response_container:
     if user_input:
-        output = query(user_input)
+        response = query(user_input)
 
         st.session_state.past.append(user_input)
-        st.session_state.generated.append(output)
+        st.session_state.generated.append(response)
 
     if st.session_state['generated']:
-
         for i in range(len(st.session_state['generated'])-1, -1, -1):
             message(st.session_state["generated"][i], key=str(i))
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
