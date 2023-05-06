7622f806252d8fea2073bb493368324da6a59e2c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-06 22:06:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,5 @@ with response_container:
     if st.session_state['generated']:
         #for i in range(len(st.session_state['generated'])-1, -1, -1):
         for i in range(len(st.session_state['generated'])):
-            message(st.session_state["generated"][i], key=str(i))
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-
-            
-
+            message(st.session_state["generated"][i], key=str(i))
