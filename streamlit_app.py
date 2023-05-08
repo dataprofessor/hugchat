5eb9aace8b32eb2025f4f2171a258acc0a24a7a8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-08 19:29:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 
+st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+
 # Sidebar contents
 with st.sidebar:
     st.title('🤗💬 HugChat App')
