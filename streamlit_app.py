a0040c34998c52bdd15981faa79eeda2fc4adc1a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-29 09:29:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,30 +26,20 @@ with st.sidebar:
     add_vertical_space(5)
     st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
 
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
 
-# Layout of input/response containers
-input_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
+# Stores AI generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state['messages'] = [{"role": "assistant", "content": "I'm HugChat, How may I help you?"}]
+# Prompt for user input and save
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-# User input
-## Function for taking user provided prompt as input
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_text()
+# Display the existing chat messages
+for message in st.session_state['messages']:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
+# Function for taking user prompt as input followed by producing AI generated responses
 def generate_response(prompt, email, passwd):
     # Hugging Face Login
     sign = Login(email, passwd)
@@ -60,14 +50,40 @@ def generate_response(prompt, email, passwd):
     response = chatbot.chat(prompt)
     return response
 
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input and hf_email and hf_pass:
-        response = generate_response(user_input, hf_email, hf_pass)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+# User input
+## Function for taking user provided prompt as input
+def get_text():
+    input_text = st.text_input("You: ", "", key="input")
+    return input_text
+## Applying the user input box
+with input_container:
+    user_input = get_text()
+
+# If last message is not from assistant, we need to generate a new response
+if st.session_state['messages'][-1]["role"] != "assistant":
+    # Call LLM
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            #r = openai.ChatCompletion.create(
+            #    model="gpt-3.5-turbo",
+            #    messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state['messages']],
+            #)
+            #response = r.choices[0].message.content
+            response = generate_response(user_input, hf_email, hf_pass)
+            st.write(response)
+
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
+
+##################
+
+#if 'generated' not in st.session_state:
+#    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+## past stores User's questions
+#if 'past' not in st.session_state:
+#    st.session_state['past'] = ['Hi!']
+
+# Layout of input/response containers
+#input_container = st.container()
+#colored_header(label='', description='', color_name='blue-30')
+#response_container = st.container()
