baa46483f839c0eb920ef913358f2c0b47fb7c2b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-29 18:02:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+# App title
+st.set_page_config(page_title="HugChat - An LLM-powered ChatBot app")
 st.title('🤗💬 HugChat App')
 
 # Hugging Face Credentials
@@ -21,14 +22,14 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 # Function for generating LLM response
-def generate_response(prompt, email, passwd):
+def generate_response(prompt_input, email, passwd):
     # Hugging Face Login
     sign = Login(email, passwd)
     cookies = sign.login()
     sign.saveCookies()
     # Create ChatBot                        
     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-    response = chatbot.chat(prompt)
+    response = chatbot.chat(prompt_input)
     return response
 
 # Prompt for user input and save
@@ -42,10 +43,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
     # Call LLM
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            
-            response = generate_response(prompt, hf_email, hf_pass)
-            
-            st.write(response)
-            
+            response = generate_response(prompt, hf_email, hf_pass) 
+            st.write(response) 
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
