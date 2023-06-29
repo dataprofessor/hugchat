d66d6becd4e4eec2c541b7fc992b3432c5e97cb8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-29 12:01:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,5 @@ if st.session_state['messages'][-1]["role"] != "assistant":
 
     #message = {"role": "assistant", "content": response}
     #st.session_state.messages.append(message)
+
+st.write(message)
