32fc824796d350d230c069f03977d1065a1aaae9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-29 18:59:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ from hugchat import hugchat
 from hugchat.login import Login
 
 # App title
-st.set_page_config(page_title="🤗💬 HugChat Chatbot")
+st.set_page_config(page_title="🤗💬 HugChat")
 
 # Hugging Face Credentials
 with st.sidebar:
-    st.title('🤗💬 HugChat Chatbot')
+    st.title('🤗💬 HugChat')
     if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
         st.success('HuggingFace Login credentials already provided!', icon='✅')
         hf_email = st.secrets['EMAIL']
