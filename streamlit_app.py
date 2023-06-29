a27d009f1eeb8c4ce89fb8f1f08b3d5ecad71570
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-29 10:51:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,26 @@
 import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 from hugchat.login import Login
 
 st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+st.title('🤗💬 HugChat App')
 
-# Sidebar contents
+# Hugging Face Credentials
 with st.sidebar:
-    st.title('🤗💬 HugChat App')
-    
     st.header('Hugging Face Login')
     hf_email = st.text_input('Enter E-mail:', type='password')
     hf_pass = st.text_input('Enter password:', type='password')
-    
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [HugChat](https://github.com/Soulter/hugging-chat-api)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
 
-    ''')
-    add_vertical_space(5)
-    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
-
-
-# Stores AI generated responses
+# Store AI generated responses
 if "messages" not in st.session_state.keys():
     st.session_state['messages'] = [{"role": "assistant", "content": "I'm HugChat, How may I help you?"}]
 
-# Prompt for user input and save
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-# Display the existing chat messages
+# Display existing chat messages
 for message in st.session_state['messages']:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# Function for taking user prompt as input followed by producing AI generated responses
+# Function for generating LLM response
 def generate_response(prompt, email, passwd):
     # Hugging Face Login
     sign = Login(email, passwd)
@@ -51,16 +31,15 @@ def generate_response(prompt, email, passwd):
     response = chatbot.chat(prompt)
     return response
 
+# Prompt for user input and save
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
 # If last message is not from assistant, we need to generate a new response
 if st.session_state['messages'][-1]["role"] != "assistant":
     # Call LLM
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            #r = openai.ChatCompletion.create(
-            #    model="gpt-3.5-turbo",
-            #    messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state['messages']],
-            #)
-            #response = r.choices[0].message.content
             response = generate_response(prompt, hf_email, hf_pass)
             st.write(response)
 
