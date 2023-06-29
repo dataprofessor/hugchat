830c981b68ef5b0b1088dbbe4f2f1f711b10766b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-29 13:34:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def generate_response(prompt, email, passwd):
     # Create ChatBot                        
     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
     response = chatbot.chat(prompt)
-    return response
+    return st.write(response)
 
 # Prompt for user input and save
 if prompt := st.chat_input():
@@ -40,7 +40,7 @@ if st.session_state['messages'][-1]["role"] != "assistant":
     # Call LLM
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_response(prompt, hf_email, hf_pass)
+            generate_response(prompt, hf_email, hf_pass)
             
             # Hugging Face Login
             #sign = Login(hf_email, hf_pass)
@@ -49,8 +49,6 @@ if st.session_state['messages'][-1]["role"] != "assistant":
             # Create ChatBot                        
             #chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
             #response = chatbot.chat(prompt)
-            
-            st.write(response)
 
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
