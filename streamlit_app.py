e60de6c5013e5844b31c853d4b2e90012df63eec
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-01 19:33:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,10 @@ def generate_response(prompt_input, email, passwd):
     # Hugging Face Login
     sign = Login(email, passwd)
     cookies = sign.login()
-    sign.saveCookies()
+    #sign.saveCookies()
     # Create ChatBot                        
-    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    #chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    chatbot = hugchat.ChatBot()
     return chatbot.chat(prompt_input)
 
 # User-provided prompt
