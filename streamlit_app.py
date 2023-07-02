5cfa862b6e15db01a7963f3526a79f51b3cf8409
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-02 10:06:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
-from PIL import Image
 
 # App title
 st.set_page_config(page_title="🤗💬 HugChat")
 
-image = Image.open('streamlit.png')
+image = 'streamlit.png'
 with st.chat_message("assistant", avatar='https://streamlit.io/images/brand/streamlit-mark-color.png' ):
     st.write('Testing ... 1, 2, 3')
 with st.chat_message("assistant", avatar=image ):
